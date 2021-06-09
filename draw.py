d44a97285db3f45158b62e946cc05ebcb8c80ba1
Gabriel Trigueiro <63242633+Yatmish@users.noreply.github.com>
2021-06-09 12:06:36 -0300

    newUpdate

--- draw.py
+++ draw.py
@@ -1,5 +1,5 @@
-import cv2
-import numpy as np
+from drawingPython.functions.imageProcess import getImage, toBinary
+from drawingPython.functions.mathFunc import get_ordered
 from time import sleep
 from pynput.mouse import Controller, Button
 
@@ -8,34 +8,6 @@ CONSTANT = 0.0000000000000000000000000000000000000000000000000000000000000000000
 
 img_path = './img/test.png'
 saved_path = './img/savedFile.png'
-
-
-def getImage(img_path, saved):
-    im = cv2.imread(img_path, 2)
-
-    im90 = cv2.rotate(im, cv2.ROTATE_90_COUNTERCLOCKWISE)
-
-    flipVertical = cv2.flip(im90, 0)
-
-    ret, bw_img = cv2.threshold(flipVertical, 128, 255, cv2.THRESH_BINARY)
-
-    cv2.imwrite(saved, bw_img)
-
-
-def toBinary(saved_path):
-    binaryImg = cv2.imread('./savedFile.png')
-
-    black = [0, 0, 0]
-
-    blackX, blackY = np.where(np.all(binaryImg == black, axis=2))
-
-    return list(zip(blackX, blackY))
-
-
-def get_ordered(points):
-    points.sort(key=lambda p: (p[0] - 0.0) ** 2 + (p[1] - 0.0)**2)
-    return points
-
 
 getImage(img_path=img_path, saved=saved_path)
 
